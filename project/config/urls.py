476de67e05dfe0e0639576253ebe9e210e4eebb0
Valentin Cabrera <valentincabrera98@gmail.com>
2024-06-23 15:49:34 -0300

    Path de la primera vista

--- project/config/urls.py
+++ project/config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views #! Importación de las vistas de views.py 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludar/', views.saludar),
+
 ]
