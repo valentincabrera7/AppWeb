3a3321eeae29212fa14463de0d45976dca8159db
Valentin Cabrera <valentincabrera98@gmail.com>
2024-06-25 16:42:44 -0300

    Template

--- project/apps/Home/views.py
+++ project/apps/Home/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 
-# Create your views here.
+def home(request):
+    return render(request, "Home/index.html")
--- project/config/urls.py
+++ project/config/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include #! INCLUDE SIRVE PARA TRAER URLS DE OTRAS 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("cliente.urls")),
+    path("", include("Home.urls")),
+    path("cliente/", include("cliente.urls")),
     path("producto/", include("producto.urls")),
     
 ]
