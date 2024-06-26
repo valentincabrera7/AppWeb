4a3a6884f5048a88588a5abe97370ae3b0208a80
Valentin Cabrera <valentincabrera98@gmail.com>
2024-06-25 22:31:08 -0300

    ADD: modelo estudiante, profesor y curso

--- project/apps/Home/models.py
+++ project/apps/Home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    correo = models.EmailField()
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=100)
+    especialidad = models.CharField(max_length=100)
+    correo = models.EmailField()
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
