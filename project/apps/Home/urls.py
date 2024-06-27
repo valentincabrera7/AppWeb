59a7f3a8d68df421340d925e2b88dd0a0a23478e
Valentin Cabrera <valentincabrera98@gmail.com>
2024-06-26 22:39:33 -0300

    refactor: division de index en componentes

--- project/apps/Home/urls.py
+++ project/apps/Home/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = "Home"
 
 urlpatterns = [
     path("", views.home, name="home"),
 ]   
+
+urlpatterns += staticfiles_urlpatterns()
--- project/apps/cliente/urls.py
+++ project/apps/cliente/urls.py
@@ -4,7 +4,7 @@ from .views import home, crear_clientes, crear_cliente, busqueda #! Importación
 app_name = "cliente"
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", home, name="home"), #! HOME DE CLIENTE 
     path("crear_clientes", crear_clientes, name="crear_clientes"),
     path("crear/", crear_cliente, name="crear"), 
     path("busqueda", busqueda, name="busqueda"),
