98ff27e1f459717a21058c91a16350715db610df
Valentin Cabrera <valentincabrera98@gmail.com>
2024-06-27 17:10:25 -0300

    delete: eliminados modelos de home

--- project/apps/Home/models.py
+++ project/apps/Home/models.py
@@ -1,24 +1,2 @@
 from django.db import models
 
-class Estudiante(models.Model):
-    nombre = models.CharField(max_length=100)
-    edad = models.IntegerField()
-    correo = models.EmailField()
-
-    def __str__(self):
-        return self.nombre
-
-class Profesor(models.Model):
-    nombre = models.CharField(max_length=100)
-    especialidad = models.CharField(max_length=100)
-    correo = models.EmailField()
-
-    def __str__(self):
-        return self.nombre
-
-class Curso(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField()
-
-    def __str__(self):
-        return self.nombre
